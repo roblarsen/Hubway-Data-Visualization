4ec251e518d7c6ff2303b7a3c61d68cd86c63d52
Rob Larsen <rob@drunkenfist.com>
2012-10-25 16:46:26 -0400

    I have the basic functionality down. You can click on a marker and get some paths. Sweet.

---
+++ src/data/data.js
@@ -0,0 +1,345 @@
+var data = {
+  "stations": [{
+    "name": "South Station",
+    "depts": "26555",
+    "destinations": [{
+      "name": "TD Garden",
+      "trips": "2690"
+    }, {
+      "name": "Lewis Wharf - Atlantic Ave",
+      "trips": "2312"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave.",
+      "trips": "1629"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+      "trips": "1413"
+    }, {
+      "name": "Cross St. at Hanover St. ",
+      "trips": "1306"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "1171"
+    }, {
+      "name": "Boylston St.  / Berkeley St. ",
+      "trips": "1009"
+    }, {
+      "name": "South Station - 700 Atlantic Ave",
+      "trips": "1008"
+    }, {
+      "name": "Seaport Hotel",
+      "trips": "869"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "trips": "746"
+    }]
+  }, {
+    "name": "TD Garden - Legends Way",
+    "depts": "21343",
+    "destinations": [{
+      "name": "South Station - 700 Atlantic Ave.",
+      "trips": "2473"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave",
+      "trips": "1367"
+    }, {
+      "name": "Congress/Sleeper",
+      "trips": "1150"
+    }, {
+      "name": "Post Office Square",
+      "trips": "1119"
+    }, {
+      "name": "TD Garden - Legends Way",
+      "trips": "1064"
+    }, {
+      "name": "Seaport Square - Seaport Blvd. at Boston Wharf",
+      "trips": "995"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "830"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+      "trips": "798"
+    }, {
+      "name": "Summer St. / Arch St. ",
+      "trips": "704"
+    }, {
+      "name": "Faneuil Hall - Union St. at North St. ",
+      "trips": "691"
+    }]
+  }, {
+    "name": "Boston Public Library - 700 Boylston St.",
+    "depts": "21334",
+    "destinations": [{
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "trips": "1937"
+    }, {
+      "name": "South Station - 700 Atlantic Ave.",
+      "trips": "989"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "814"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St. ",
+      "trips": "749"
+    }, {
+      "name": "Tremont St. / West St.",
+      "trips": "617"
+    }, {
+      "name": "Newbury St. / Hereford St. ",
+      "trips": "579"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+      "trips": "556"
+    }, {
+      "name": "Beacon St. / Mass Ave",
+      "trips": "541"
+    }, {
+      "name": "Fanueil Hall - Union St. at North St. ",
+      "trips": "539"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St. ",
+      "trips": "535"
+    }]
+  }, {
+    "name": "Boylston St. at Arlington St.",
+    "depts": "16366",
+    "destinations": [{
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "1568"
+    }, {
+      "name": "South Station - 700 Atlantic Ave.",
+      "trips": "1273"
+    }, {
+      "name": "TD Garden - Legends Way",
+      "trips": "706"
+    }, {
+      "name": "Newbury St. / Hereford St. ",
+      "trips": "694"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "trips": "618"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave.",
+      "trips": "567"
+    }, {
+      "name": "Kenmore Sq. /Comm Ave",
+      "trips": "526"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St. ",
+      "trips": "482"
+    }, {
+      "name": "Tremont St. / West St. ",
+      "trips": "453"
+    }, {
+      "name": "Mayor Thomas M. Menino  - Government Center",
+      "trips": "432"
+    }]
+  }, {
+    "name": "Back Bay / South End Station",
+    "depts": "14456",
+    "destinations": [{
+      "name": "Washington St. at Rutland St. Boston, Ma",
+      "trips": "1253"
+    }, {
+      "name": "Tremont St / W Newton St. ",
+      "trips": "794"
+    }, {
+      "name": "Back Bay / South End Station",
+      "trips": "700"
+    }, {
+      "name": "Roxbury Crossing Station",
+      "trips": "663"
+    }, {
+      "name": "Columbus Ave. at Mass Ave.",
+      "trips": "659"
+    }, {
+      "name": "Christian Science Plaza",
+      "trips": "594"
+    }, {
+      "name": "Washington St. at Waltham St. ",
+      "trips": "479"
+    }, {
+      "name": "Prudential Center / Belvidere",
+      "trips": "429"
+    }, {
+      "name": "Boston Medical Center - 721 Mass. Ave",
+      "trips": "399"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+      "trips": "394"
+    }]
+  }, {
+    "name": "Charles Circle - Charles St. at Cambridge St.",
+    "depts": "13616",
+    "destinations": [{
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+      "trips": "1479"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St.",
+      "trips": "869"
+    }, {
+      "name": "Beacon St / Mass Ave",
+      "trips": "770"
+    }, {
+      "name": "TD Garden - Legends Way",
+      "trips": "551"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "549"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St. ",
+      "trips": "462"
+    }, {
+      "name": "Kenmore Sq / Comm Ave",
+      "trips": "446"
+    }, {
+      "name": "Stuart St. at Charles St.",
+      "trips": "418"
+    }, {
+      "name": "Cross St. at Hanover St. ",
+      "trips": "409"
+    }, {
+      "name": "Mayor Thomas M. Menino - Government Center",
+      "trips": "326"
+    }]
+  }, {
+    "name": "Kenmore Square / Comm Ave. Total Departures:",
+    "depts": "13108",
+    "destinations": [
+
+    {
+      "name": "Agganis Arena - 925 Comm. Ave.",
+      "trips": "1066"
+    }, {
+      "name": "Kenmore Sq / Comm Ave",
+      "trips": "956"
+    }, {
+      "name": "B.U. Central - 725 Comm Ave",
+      "trips": "951"
+    }, {
+      "name": "Packard's Corner - Comm. Ave at Brighton Ave. ",
+      "trips": "829"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "trips": "503"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "trips": "469"
+    }, {
+      "name": "Landmark Centre",
+      "trips": "452"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+      "trips": "417"
+    }, {
+      "name": "Newbury St / Hereford St.",
+      "trips": "389"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St.",
+      "trips": "385"
+    }]
+  }, {
+    "name": "Beacon St. / Mass Ave.",
+    "depts": "12589",
+    "destinations": [{
+      "name": "Beacon St. / Mass. Ave",
+      "trips": "1322"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+      "trips": "660"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St.",
+      "trips": "618"
+    }, {
+      "name": "Kenmore Sq. / Comm Ave",
+      "trips": "545"
+    }, {
+      "name": "Boylston St. at Arlington St.",
+      "trips": "455"
+    }, {
+      "name": "MIT at Mass Ave / Amherst St.",
+      "trips": "446"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St.",
+      "trips": "384"
+    }, {
+      "name": "TD Garden - Legends Way",
+      "trips": "333"
+    }, {
+      "name": "Christian Science Plaza",
+      "trips": "324"
+    }, {
+      "name": "Boylston / Mass Ave",
+      "trips": "308"
+    }]
+  }, {
+    "name": "Lewis Wharf - Atlantic Ave.",
+    "depts": "12288",
+    "destinations": [{
+      "name": "South Station - 700 Atlantic Ave.",
+      "trips": "2369"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave",
+      "trips": "895"
+    }, {
+      "name": "Lewis Wharf - Atlantic Ave.",
+      "trips": "876"
+    }, {
+      "name": "TD Garden - Legends Way",
+      "trips": "656"
+    }, {
+      "name": "Post Office Square",
+      "trips": "524"
+    }, {
+      "name": "Congress / Sleeper",
+      "trips": "473"
+    }, {
+      "name": "Seaport Square - Seaport Blvd. at Boston Wharf",
+      "trips": "464"
+    }, {
+      "name": "Fanueil Hall - Union St. at North St. ",
+      "trips": "448"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+      "trips": "431"
+    }, {
+      "name": "Chinatown Gate Plaza - Surface Rd. at Beach St. ",
+      "trips": "413"
+    }]
+  }, {
+    "name": "Newbury St. / Hereford St.",
+    "depts": "12262",
+    "destinations": [{
+      "name": "Newbury St. / Hereford St. ",
+      "trips": "1159"
+    }, {
+      "name": "Boylston St. at Arlington St.",
+      "trips": "626"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "trips": "549"
+    }, {
+      "name": "Washington St. at Rutland St. Boston, Ma",
+      "trips": "465"
+    }, {
+      "name": "Kenmore Sq / Comm Ave.",
+      "trips": "437"
+    }, {
+      "name": "Yawkey Way at Boylston St. ",
+      "trips": "397"
+    }, {
+      "name": "Packard's Corner - Comm Ave. at Brighton Ave.",
+      "trips": "361"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+      "trips": "322"
+    }, {
+      "name": "Landmark Centre",
+      "trips": "307"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+      "trips": "299"
+    }]
+  }]
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,88 @@
-
-;(function (window, document, $, undefined) {
+;
+(function(window, document, $, undefined) {
   "use strict";
-  if (window.HW === undefined) {
+  if(window.HW === undefined) {
     window.HW = {};
   };
   var HW = window.HW;
   HW.common = {
-    init : function(){
-      console.log( "common.init" );
-      $( "#map" ).height($( window ).height() );
+    init: function() {
+      $("#map").height($(window).height());
       var GM = google.maps,
-          defaultPosition = new GM.LatLng( 42.3520, -71.0560 ),
-          mapOptions = {
-            zoom: 16,
-            center: defaultPosition,
-            mapTypeId: GM.MapTypeId.ROADMAP
-          },
-          map = new GM.Map( document.getElementById('map'), mapOptions),
-          success = function( data ){
-            var position = new GM.LatLng( data.coords.latitude, data.coords.longitude ),
-                niceAddress = "Your location",
-                geocoder = new GM.Geocoder();
-            geocoder.geocode(
-              { 'latLng': position }, 
-              function( results, status ) {
-                if ( status == GM.GeocoderStatus.OK ) {
-                  if ( results[0] ) {
-                    niceAddress = results[0].formatted_address;
-                  }
-                } 
-            });  
-            map.setCenter(defaultPosition);
-            var polylineOptions = new GM.Polyline({
-                 strokeColor: '#FF0000',
-                 strokeOpacity: .7,
-                 strokeWeight: 10
-                }),
-                DS = new GM.DirectionsService(),
-                DD = new GM.DirectionsRenderer( { polylineOptions : polylineOptions });
-            DD.setMap(map);
+        defaultPosition = new GM.LatLng(42.3520, -71.0560),
+        mapOptions = {
+          zoom: 12,
+          center: defaultPosition,
+          mapTypeId: GM.MapTypeId.ROADMAP
+        },
+        map = new GM.Map(document.getElementById('map'), mapOptions),
+        geocoder = new GM.Geocoder(),
+        position, endPosition, marker, polylineOptions, DD, DS;
+      $("#map").data("map", map)
+      for(var i = 0, len = data.stations.length; i < len; i++) {
+        var ii = i;
+        geocoder.geocode({
+          'address': data.stations[i].name
+        }, function(results, status) {
+          console.log(results);
+          if(results) {
+            position = new GM.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+            marker = new GM.Marker({
+              position: position,
+              map: map,
+              title: results[0].formatted_name,
+              data: data.stations[ii].destinations
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+              for(var j = 0; j < 10; j++) {
+                var name = this.data[j].name;
+                geocoder.geocode({
+                  'address': name
+                }, function(results, status) {
+                  console.log(results);
+                  if(results) {
+                    endPosition = new GM.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+                    HW.common.renderer(position, endPosition)
+                    marker = new GM.Marker({
+                      position: endPosition,
+                      map: map,
+                      title: results[0].formatted_name,
+                    });
 
-            var request = {
-              origin:defaultPosition,
-              destination:position,
-              travelMode: GM.TravelMode.BICYCLING
-            };
-            DS.route(request, function(result, status) {
-              if (status == GM.DirectionsStatus.OK) {
-                DD.setDirections(result);
+                  }
+                });
+
               }
             });
-          },
-          failure = function( error ){
-          //handle errors
-          };
-      if ( Modernizr.geolocation ){
-        navigator.geolocation.getCurrentPosition( success, failure, {timeout:5000} ) ;      
-      }    
+          }
+        });
+      }
+    },
+    renderer: function(start, end) {
+      var GM = google.maps,
+        polylineOptions, DD, DS, map = $("#map").data("map");
+      var polylineOptions = new GM.Polyline({
+        strokeColor: '#FF0000',
+        strokeOpacity: .7,
+        strokeWeight: 10
+      }),
+        DS = new GM.DirectionsService(),
+        DD = new GM.DirectionsRenderer({
+          polylineOptions: polylineOptions
+        });
+      DD.setMap(map);
+
+      var request = {
+        origin: start,
+        destination: end,
+        travelMode: GM.TravelMode.BICYCLING
+      };
+      DS.route(request, function(result, status) {
+        if(status == GM.DirectionsStatus.OK) {
+          DD.setDirections(result);
+        }
+      });
     }
+
   }
-}( window, document, jQuery ));
+}(window, document, jQuery));
