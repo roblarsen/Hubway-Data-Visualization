6b0fe7adf18561571f7c6fd68767d1eab95054de
Rob Larsen <rob@drunkenfist.com>
2012-10-26 11:38:45 -0400

    adding some demo pages

---
+++ demo/data/data.js
@@ -0,0 +1,533 @@
+var data = {
+  "stations": [{
+    "name": "South Station",
+    "latLng": [42.352175,-71.055547],
+    "depts": "26555",
+    "destinations": [{
+      "name": "TD Garden",
+      "latLng": [42.366231,-71.060868],
+      "trips": "2690"
+    }, {
+      "name": "Lewis Wharf - Atlantic Ave",
+      "latLng": [42.363871,-71.050877],
+      "trips": "2312"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave.",
+      "latLng":[42.357143,-71.050699],
+      "trips": "1629"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+      "latLng": [42.35977,-71.051601],
+      "trips": "1413"
+    }, {
+      "name": "Cross St. at Hanover St. ",
+      "latLng": [42.362811,-71.056067],
+      "trips": "1306"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+      "latLng": [42.352096,-71.070378],
+      "trips": "1171"
+    }, {
+      "name": "Boylston St.  / Berkeley St. ",
+      "latLng": [42.350989,-71.073644],
+      "trips": "1009"
+    }, 
+    // {
+    //   "name": "South Station - 700 Atlantic Ave",
+    //   "latLng": [42.352175,-71.055547],
+    //   "trips": "1008"
+    // }, 
+    {
+      "name": "Seaport Hotel",
+      "latLng": [42.348833,-71.041747],
+      "trips": "869"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+      "latLng": [42.349673,-71.077303],
+      "trips": "746"
+    }]
+  }, {
+    "name": "TD Garden - Legends Way",
+    "latLng": [42.366231,-71.060868],
+    "depts": "21343",
+    "destinations": [{
+      "name": "South Station - 700 Atlantic Ave.",//done
+      "latLng": [42.352175, -71.055547],
+      "trips": "2473"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave.",//done
+      "latLng":[42.357143,-71.050699],
+      "trips": "1367"
+    }, {
+      "name": "Congress/Sleeper",//done
+      "latLng": [42.3511,-71.0496],
+      "trips": "1150"
+    }, {
+      "name": "Post Office Square",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1119"
+    }, {
+      "name": "TD Garden - Legends Way",//done
+      "latLng": [42.366231,-71.060868],
+      "trips": "1064"
+    }, {
+      "name": "Seaport Square - Seaport Blvd. at Boston Wharf",//done
+      "latLng": [42.351821,-71.045461],
+      "trips": "995"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "830"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "798"
+    }, {
+      "name": "Summer St. / Arch St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "704"
+    }, {
+      "name": "Faneuil Hall - Union St. at North St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "691"
+    }]
+  }, {
+    "name": "Boston Public Library - 700 Boylston St.",
+    "depts": "21334",
+    "destinations": [{
+      "name": "Boston Public Library - 700 Boylston St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1937"
+    }, {
+      "name": "South Station - 700 Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "989"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "814"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "749"
+    }, {
+      "name": "Tremont St. / West St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "617"
+    }, {
+      "name": "Newbury St. / Hereford St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "579"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "556"
+    }, {
+      "name": "Beacon St. / Mass Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "541"
+    }, {
+      "name": "Fanueil Hall - Union St. at North St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "539"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "535"
+    }]
+  }, {
+    "name": "Boylston St. at Arlington St.",
+    "depts": "16366",
+    "destinations": [{
+      "name": "Boylston St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1568"
+    }, {
+      "name": "South Station - 700 Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1273"
+    }, {
+      "name": "TD Garden - Legends Way",
+
+      "latLng": [42.366231,-71.060868],
+      "trips": "706"
+    }, {
+      "name": "Newbury St. / Hereford St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "694"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "618"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "567"
+    }, {
+      "name": "Kenmore Sq. /Comm Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "526"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "482"
+    }, {
+      "name": "Tremont St. / West St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "453"
+    }, {
+      "name": "Mayor Thomas M. Menino  - Government Center",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "432"
+    }]
+  }, {
+    "name": "Back Bay / South End Station",
+    "depts": "14456",
+    "destinations": [{
+      "name": "Washington St. at Rutland St. Boston, Ma",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1253"
+    }, {
+      "name": "Tremont St / W Newton St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "794"
+    }, {
+      "name": "Back Bay / South End Station",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "700"
+    }, {
+      "name": "Roxbury Crossing Station",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "663"
+    }, {
+      "name": "Columbus Ave. at Mass Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "659"
+    }, {
+      "name": "Christian Science Plaza",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "594"
+    }, {
+      "name": "Washington St. at Waltham St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "479"
+    }, {
+      "name": "Prudential Center / Belvidere",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "429"
+    }, {
+      "name": "Boston Medical Center - 721 Mass. Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "399"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "394"
+    }]
+  }, {
+    "name": "Charles Circle - Charles St. at Cambridge St.",
+    "depts": "13616",
+    "destinations": [{
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1479"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "869"
+    }, {
+      "name": "Beacon St / Mass Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "770"
+    }, {
+      "name": "TD Garden - Legends Way",
+
+      "latLng": [42.366231,-71.060868],
+      "trips": "551"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "549"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "462"
+    }, {
+      "name": "Kenmore Sq / Comm Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "446"
+    }, {
+      "name": "Stuart St. at Charles St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "418"
+    }, {
+      "name": "Cross St. at Hanover St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "409"
+    }, {
+      "name": "Mayor Thomas M. Menino - Government Center",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "326"
+    }]
+  }, {
+    "name": "Kenmore Square / Comm Ave. Total Departures:",
+    "depts": "13108",
+    "destinations": [
+
+    {
+      "name": "Agganis Arena - 925 Comm. Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1066"
+    }, {
+      "name": "Kenmore Sq / Comm Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "956"
+    }, {
+      "name": "B.U. Central - 725 Comm Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "951"
+    }, {
+      "name": "Packard's Corner - Comm. Ave at Brighton Ave. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "829"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "503"
+    }, {
+      "name": "Boylston St. at Arlington St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "469"
+    }, {
+      "name": "Landmark Centre",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "452"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "417"
+    }, {
+      "name": "Newbury St / Hereford St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "389"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "385"
+    }]
+  }, {
+    "name": "Beacon St. / Mass Ave.",
+    "depts": "12589",
+    "destinations": [{
+      "name": "Beacon St. / Mass. Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1322"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "660"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "618"
+    }, {
+      "name": "Kenmore Sq. / Comm Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "545"
+    }, {
+      "name": "Boylston St. at Arlington St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "455"
+    }, {
+      "name": "MIT at Mass Ave / Amherst St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "446"
+    }, {
+      "name": "The Esplanade - Beacon St. at Arlington St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "384"
+    }, {
+      "name": "TD Garden - Legends Way",
+
+      "latLng": [42.366231,-71.060868],
+      "trips": "333"
+    }, {
+      "name": "Christian Science Plaza",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "324"
+    }, {
+      "name": "Boylston / Mass Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "308"
+    }]
+  }, {
+    "name": "Lewis Wharf - Atlantic Ave.",
+    "depts": "12288",
+    "destinations": [{
+      "name": "South Station - 700 Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "2369"
+    }, {
+      "name": "Rowes Wharf - Atlantic Ave",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "895"
+    }, {
+      "name": "Lewis Wharf - Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "876"
+    }, {
+      "name": "TD Garden - Legends Way",
+
+      "latLng": [42.366231,-71.060868],
+      "trips": "656"
+    }, {
+      "name": "Post Office Square",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "524"
+    }, {
+      "name": "Congress / Sleeper",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "473"
+    }, {
+      "name": "Seaport Square - Seaport Blvd. at Boston Wharf",//done
+      "latLng": [42.351821,-71.045461],
+      "trips": "464"
+    }, {
+      "name": "Fanueil Hall - Union St. at North St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "448"
+    }, {
+      "name": "Aquarium Station - 200 Atlantic Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "431"
+    }, {
+      "name": "Chinatown Gate Plaza - Surface Rd. at Beach St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "413"
+    }]
+  }, {
+    "name": "Newbury St. / Hereford St.",
+    "depts": "12262",
+    "destinations": [{
+      "name": "Newbury St. / Hereford St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "1159"
+    }, {
+      "name": "Boylston St. at Arlington St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "626"
+    }, {
+      "name": "Boston Public Library - 700 Boylston St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "549"
+    }, {
+      "name": "Washington St. at Rutland St. Boston, Ma",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "465"
+    }, {
+      "name": "Kenmore Sq / Comm Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "437"
+    }, {
+      "name": "Yawkey Way at Boylston St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "397"
+    }, {
+      "name": "Packard's Corner - Comm Ave. at Brighton Ave.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "361"
+    }, {
+      "name": "Charles Circle - Charles St. at Cambridge St.",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "322"
+    }, {
+      "name": "Landmark Centre",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "307"
+    }, {
+      "name": "Stuart St. at Charles St. ",
+
+      "latLng": [42.352175, -71.055547],
+      "trips": "299"
+    }]
+  }]
+}
---
+++ demo/js/plugins.js
@@ -0,0 +1,41 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = window.console || {};
+
+    while (length--) {
+        // Only stub undefined methods.
+        console[methods[length]] = console[methods[length]] || noop;
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+UTIL = {
+  fire : function(func,funcname, args){
+    var namespace = HW;  // indicate your obj literal namespace here
+    funcname = (funcname === undefined) ? 'init' : funcname;
+    if (func !== '' && namespace[func] && typeof namespace[func][funcname] == 'function'){
+      namespace[func][funcname](args);
+    } 
+  }, 
+  loadEvents : function(){
+    var bodyId = document.body.id;
+    // hit up common first.
+    UTIL.fire('common');
+    // do all the classes too.
+    $.each(document.body.className.split(/\s+/),function(i,classnm){
+      UTIL.fire(classnm);
+      UTIL.fire(classnm,bodyId);
+    });
+    UTIL.fire('common','finalize');
+  } 
+}; 
+// kick it all off here 
+$(document).ready(UTIL.loadEvents);
