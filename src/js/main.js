ca4fdb6366db59bbf3b4a43f3c342b552eeea76b
Rob Larsen <rob@drunkenfist.com>
2013-03-10 23:35:32 -0400

    Colors and line scale completed.
    added a function to calculate distance between two points and managed to
    uncover the distance of the route buried in the response.

--- src/js/main.js
+++ src/js/main.js
@@ -39,6 +39,25 @@
               title: data.stations[i].name,
               data: data.stations[i].destinations
             });
+            //http://www.movable-type.co.uk/scripts/latlong.html
+            //http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+                  function distance(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
             var j = 0, len =data.stations[i].destinations.length, rendertimer;
             function newrender(){
               if (j == len){
@@ -52,21 +71,37 @@
         function createRoutes( i, j, start) {
           start =position;
           end =data.stations[i].destinations[j].latLng;
-          HW.common.renderer(start,end, map);
+
+          var trips =data.stations[i].destinations[j].trips;
+          HW.common.renderer(start,end, trips, j);
         }
           
         
        }  
      
     },
-    renderer: function(start, end) {
+    renderer: function(start, end, trips, index) {
       var GM = google.maps,
-        polylineOptions, DD, DS, map = $("#map").data("map");
+        polylineOptions, DD, DS, map = $("#map").data("map"),
+        colors = ["#B22937",
+        "#DE5003",
+        "#80C837",
+        "#229F6E",
+        "#60B6CA",
+        "#6F6DA7",
+        "#1F1D6D",
+        "#80529A",
+        "#A6358C",
+        "#A2395B"
+
+
+        ];
+
       end = new GM.LatLng(end[0],end[1]);
       var polylineOptions = new GM.Polyline({
-        strokeColor: '#FF0000',
+        strokeColor: colors[index],
         strokeOpacity: .4,
-        strokeWeight: 10
+        strokeWeight: trips/100
       }),
         DS = new GM.DirectionsService(),
         DD = new GM.DirectionsRenderer({
@@ -78,11 +113,12 @@
         destination: end,
         travelMode: GM.TravelMode.BICYCLING
       };
+
       DS.route(request, function(result, status) {
         if(status == GM.DirectionsStatus.OK) {
           DD.setDirections(result);
+          //console.log(result.trips[0].routes[0].distance.value);
         } else {
-          console.log("throttle")
         }
       });
     }
